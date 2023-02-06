210ec89fed342695c18ee4357e845ba476c67f14
BenB7 <ben.bright@students.plymouth.ac.uk>
2023-02-06 10:01:22 +0000

    Update mux2_structural.sv

--- tasks/Task215-2-to-1-multiplexer/mux2_structural.sv
+++ tasks/Task215-2-to-1-multiplexer/mux2_structural.sv
@@ -1,6 +1,13 @@
 module mux2_structural (output logic y, input logic a, b, s);
 
-// Write HDL here
+//Internal wires
+wire invS;
+wire term1;
+wire term3;
 
-
+//Placing and connecting the gates
+not u1 (invS, S);
+and u2 (term1, invS, A); 
+and u3 (term3, S, B);
+or u4 (Y, term1, term3);
 endmodule
