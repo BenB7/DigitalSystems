26d56bbfd307fef289c7c5de2ffd787ff27c42e9
BenB7 <ben.bright@students.plymouth.ac.uk>
2023-02-13 10:23:43 +0000

    Task 220

--- tasks/Task220-Shift_and_Rotate/rotateN_tb.sv
+++ tasks/Task220-Shift_and_Rotate/rotateN_tb.sv
@@ -1,1 +1,20 @@
 module rotateN_tb;
+
+logic DIR;
+logic [7:0] X,Y;
+parameter N=8;
+
+rotateN u1 (Y,X,DIR);
+
+initial
+begin
+DIR=1;
+X=8'b10101100;
+#10ps;
+     begin
+     assert (Y ==({ X[0], X[N-1:1] } || { X[N-2:0], X[N-1] })) $display("PASS");
+          else $display ("FAIL");
+
+    end
+end
+endmodule
