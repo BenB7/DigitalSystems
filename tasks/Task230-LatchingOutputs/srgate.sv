539728141b32e44afeafae92a20f4e0642f4442b
BenB7 <ben.bright@students.plymouth.ac.uk>
2023-02-20 15:32:45 +0000

    Task 230/234

--- tasks/Task230-LatchingOutputs/srgate.sv
+++ tasks/Task230-LatchingOutputs/srgate.sv
@@ -1,18 +1,15 @@
 module srgate (output logic Q, Qbar, input logic S, R);
 
-always_latch
+always_comb
 begin
 	if ( (S==1) && (R==0) )
 		{Q, Qbar} <= 2'b10;
 	else if ( (S == 0) && (R == 1) )
 		{Q, Qbar} <= 2'b01;
-	else if ( (S == 1'b1) && (R == 1'b1) )
+	else if ( (S == 1) && (R == 1) )
 		{Q, Qbar} <= 2'bzz;
-	// No coverage of the input combination 0 0 !!!!
+    // No coverage of the input combination 0,0!!!
 end
-
 endmodule
-
-
 
 
