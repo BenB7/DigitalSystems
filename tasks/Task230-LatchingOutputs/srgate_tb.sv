b9a99700543b2a68762067656f8b4aa7678b2112
BenB7 <ben.bright@students.plymouth.ac.uk>
2023-02-20 10:57:20 +0000

    Update srgate_tb.sv

--- tasks/Task230-LatchingOutputs/srgate_tb.sv
+++ tasks/Task230-LatchingOutputs/srgate_tb.sv
@@ -5,11 +5,14 @@ logic S, R, Q, Qbar;
 srgate u1 (Q, Qbar, S, R);
 
 initial
-	begin
+begin
+S=1;
+R=0;
+#10ps;
 
 	assert (((S==1)&&(R==0)) {Q, Qbar} <= 2'b10) || (((S==0)&&(R==1)) {Q, Qbar} <= 2'b01) || (((S==1)&&(R==1)) {Q, Qbar} <= 2'bzz);
 	$display("PASS");
 		else $display ("FAIL");
 
-	end
+end
 endmodule
